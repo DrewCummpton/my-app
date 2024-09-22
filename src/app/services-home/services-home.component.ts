7d913a59d42093bce708e55d29475a970b300e47
Drew Cumpton <dcumpto@wgu.edu>
2024-09-22 12:41:23 -0600

    5.11 Directives

--- src/app/services-home/services-home.component.ts
+++ src/app/services-home/services-home.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HighlightDirective } from './highlight.directive';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-services-home',
   standalone: true,
-  imports: [HighlightDirective],
+  imports: [HighlightDirective, NgFor, NgIf],
   templateUrl: './services-home.component.html',
   styleUrl: './services-home.component.css'
 })
-export class ServicesHomeComponent {
 
+export class ServicesHomeComponent implements OnInit {
+  bankingServices: any[] = [
+    {
+      "type": "Issue business loans",
+      "years": 2
+    },
+    {
+      "type": "Provide periodic reports and analysis",
+      "years": 7
+    },
+    {
+      "type": "Open new checking and/or saving accounts",
+      "years": 5
+    },
+    {
+      "type": "Monitor client accounts and keep information up-to-date",
+      "years": 3
+    },
+    {
+      "type": "Resolve debit and credit issues",
+      "years": 2
+    }
+  ];
+
+  ngOnInit(): void { }
 }
