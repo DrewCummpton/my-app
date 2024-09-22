1968bccfa115204cddede448887baa5d6b052caa
Drew Cumpton <dcumpto@wgu.edu>
2024-09-22 06:47:19 -0600

    Introducing error

--- src/app/services-home/highlight.directive.ts
+++ src/app/services-home/highlight.directive.ts
@@ -7,6 +7,13 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightDirective {
 
   constructor(el: ElementRef) { 
-    el.nativeElement.style.backgroundColor = 'purple';
+    try {
+      console.log(el);
+      el.nativeElement.style.backgroundColor = 'purple';
+      throw new Error('An error has occurred');
+    }
+    catch (e) {
+      console.error(e);
+    }
   }
-}
+};
